02f7256eb60b0e4f7e0d8805336c25d8c6b715fb
Luan <timpsembs.7@gmail.com>
2024-05-07 00:10:46 -0300

    latest

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/telefones')
+def telefones():
+    return render_template('telefones.html')
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
